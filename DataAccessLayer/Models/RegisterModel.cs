bc5ae2f4426e881c5341e2670260476ac0ed0b80
Giridharan R <giridharan2198@gmail.com>
2020-03-12 08:25:05 +0530

    Add project files.

---
+++ DataAccessLayer/Models/RegisterModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Web.Mvc.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace DataAccessLayer.Models
+{
+    public class RegisterModel {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        //public string Type { get; set; }
+        public DropDownListClass Type { get; set; }
+        public string BankName { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string Contact { get; set; }
+
+        public string Service { get; set; }
+        public string Location { get; set; }
+
+        //public SelectList TypeOfUser { get; set; }
+    }
+    public class DropDownListClass
+    {
+        public string ValueSelected { get; set; }
+        public SelectList TypeOfUser { get; set; }
+    }
+}
---
+++ HomeServices/Controllers/AuthenticateController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+
+namespace HomeServices.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        // GET: Authenticate
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        
+        public ActionResult Register()
+        {
+            RegisterModel UserModel = new RegisterModel();
+            UserModel.Type = new DropDownListClass();
+            UserModel.Type.TypeOfUser = new SelectList(new List<string> { "Customer", "Service Provider" });
+            return View(UserModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel User) {
+            User.Type = new DropDownListClass();
+            User.Type.TypeOfUser = new SelectList(new List<string> { "Customer", "Service Provider" });
+            return RedirectToAction("Login","Authenticate");
+        }
+    }
+}
